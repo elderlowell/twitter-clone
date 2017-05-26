d8cbbfff3e102abb815633422f87a44c41ecb3d3
Jonathan Lowell <elderlowell@gmail.com>
2017-05-25 20:46:11 -0600

    Finished part 1

--- js/animations.js
+++ js/animations.js
@@ -4,18 +4,21 @@ $(document).ready(function(){
   $('#char-count').hide();
   $('#tweet-submit').hide();
 
-  $('.tweet-compose').on('focus',  function() {
+  $('.tweet-compose').focus(function() {
+    var height = 25;
+    $(this).css('height', height*2);
     $('#char-count').show();
     $('#tweet-submit').show();
   });
 
-  $('.tweet-compose').on('blur',  function() {
-    var currentCount = $(this).val().length;
-    if (currentCount === 0) {
-      $('#char-count').hide();
-      $('#tweet-submit').hide();
-    }
-  });
+  // $('.tweet-compose').blur(function() {
+  //   $(this).css('height', 25);
+  //   var currentCount = $(this).val().length;
+  //   if (currentCount === 0) {
+  //     $('#char-count').hide();
+  //     $('#tweet-submit').hide();
+  //   }
+  // });
 
   $('.tweet-compose').keyup(function() {
     var currentCount = $(this).val().length;
@@ -27,10 +30,27 @@ $(document).ready(function(){
     if (currentCount > 10) {
       $('#char-count').css('color', 'inherit');
     }
-    if (currentCount > 140) {
+    if (currentCount < 0) {
       $('#tweet-submit').prop('disabled', true);
     }
+    if (currentCount >= 0) {
+      $('#tweet-submit').prop('disabled', false);
+    }
   });
+
+  $('#tweet-submit').on('click', function insertTweet() {
+    $('#stream').prepend(function() {
+      var tweetClone = $('.tweet').first().clone();
+      tweetClone.find('.avatar').attr('src', 'img/alagoon.jpg');
+      tweetClone.find('.fullname').html('Jonathan Lowell');
+      tweetClone.find('.username').html('@elderlowell');
+      tweetClone.find('.tweet-text').html($('.tweet-compose').val());
+      $('.tweet-compose').val('');
+      $('.tweet-compose').css('height', 25);
+      return tweetClone;
+    });
+  });
+
 
 
 });
